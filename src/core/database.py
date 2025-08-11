5610715bb77cef249f3363f51ef681aa0157e739
diunkz <noahdiunkz@gmail.com>
2025-08-10 22:34:29 -0400

    db connection

---
+++ src/core/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Define o caminho para o arquivo do banco de dados SQLite
+# Ele será criado na pasta 'data' na raiz do projeto
+DATABASE_URL = "sqlite:///./data/books.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base que nossos modelos ORM irão herdar
+Base = declarative_base()
