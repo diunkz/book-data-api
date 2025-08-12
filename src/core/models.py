0493f6422f90868186804eae26e8e32189c756aa
diunkz <noahdiunkz@gmail.com>
2025-08-12 19:21:36 -0400

    new class User

--- src/core/models.py
+++ src/core/models.py
@@ -20,3 +20,12 @@ class Book(Base):
     description = Column(Text)
     image_url = Column(String(255))
     source_page = Column(Integer)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    is_active = Column(Boolean, default=True)
